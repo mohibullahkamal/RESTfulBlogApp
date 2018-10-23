577264d7089e92808ba3f66265dbafa3ee6a2d26
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-10-23 19:30:06 +0000

    Server running check

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var bodyParser = require("body-parser"),
 mongoose       = require("mongoose"),
 express        = require("express"),
-app            = express(),
+app            = express();
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoder({extended: true}));
+
+// title
+// image
+// body
+// created
 
 
-
-
+// check whether our app is listening
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("SERVER IS RUNNING!!");
+})
