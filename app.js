55010489addb4fd677a0923fb557a036f52392b1
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-10-23 21:01:24 +0000

    Add mongoose model

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ mongoose       = require("mongoose"),
 express        = require("express"),
 app            = express();
 
+
+// APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+// we create mongoose schema -> MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+
+// convert the mongoose schema into model
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+// RESTFUL ROUTES
+
 
 // title
 // image
