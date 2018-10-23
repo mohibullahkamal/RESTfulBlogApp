22887a6fb108771f087ab1aef536f40a626faaad
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-10-23 20:38:39 +0000

    Error fix

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app            = express();
 
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
-app.use(express.static("public"));git 
-app.use(bodyParser.urlencoder({extended: true}));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // title
 // image
@@ -17,4 +17,5 @@ app.use(bodyParser.urlencoder({extended: true}));
 // check whether our app is listening
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNNING!!");
-})
+});
+
